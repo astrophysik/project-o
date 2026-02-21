2332f301ae61aa6cf6960f84fc03c11fcb96e975
Vadim Sadokhov <v.sadokhov@vk.team>
2026-02-21 12:51:17 +0300

    small fix comment

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -4,7 +4,7 @@
 
 // General todo:
 // - add read file to string
-// - add parse tok_int, tok_real (int.int) , tok_bool
+// - add parse tok_int, tok_real (int.int) , tok_bool, others
 // - add parse names, keywords (parse name => after check is name is keyword and which by swich)
 // - add skip comments
 // - now token value is just string. Maybe it should be redone by variant or virtual inheritance
