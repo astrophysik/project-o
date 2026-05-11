6a38f27a6183ef84a81ab7748672b97d3c89fa54
Gleb Lyulin <lulin.gleb@yandex.ru>
2026-05-11 21:12:08 +0300

    add codegen

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -1,12 +1,14 @@
+#include <filesystem>
 #include <fstream>
 #include <print>
 #include <string>
 
 #include "compiler/analysis/print/ast-print.h"
+#include "compiler/analysis/print/codegen-ast-print.h"
+#include "compiler/analysis/semantic/semantic-check.h"
+#include "compiler/codegen/llvm/llvm-codegen.h"
 #include "compiler/lexer/lexer.h"
 #include "compiler/parser/parser.h"
-#include "compiler/analysis/semantic/semantic-check.h"
-#include "compiler/analysis/print/codegen-ast-print.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -22,7 +24,23 @@ int main(int argc, char* argv[]) {
         auto parser = parser::parser(tokens_res);
         auto parsing_ast = parser.parse();
         auto semantic_ast = analysis::semantic::check_program(parsing_ast, argv[1], file_content);
-        analysis::print_codegen_ast(semantic_ast);
+
+        codegen::llvm_ir::llvm_codegen ir_gen{std::string(argv[1])};
+        ir_gen.emit(*semantic_ast);
+        std::print("{}\n", ir_gen.ir_to_string());
+
+        std::filesystem::path input_path(argv[1]);
+        auto ir_path = input_path;
+        ir_path.replace_extension(".ll");
+        auto obj_path = input_path;
+        obj_path.replace_extension(".o");
+
+        if (!ir_gen.write_ir_file(ir_path.string())) {
+            std::print(stderr, "Failed to write LLVM IR to {}\n", ir_path.string());
+        }
+        if (!ir_gen.write_object_file(obj_path.string())) {
+            std::print(stderr, "Failed to write object file to {}\n", obj_path.string());
+        }
     } catch (std::exception& e) {
         std::cerr << "Compilation error : \n" << e.what();
     }
